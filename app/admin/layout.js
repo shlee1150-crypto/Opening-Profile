858508a743a8ec746d47f3b36bf9a3cd9d780c90
shlee1150-crypto <shlee1150@gmail.com>
2026-08-25 17:06:20 +0900

    Create layout.js

---
+++ app/admin/layout.js
@@ -0,0 +1,17 @@
+export const metadata = {
+  title:
+    "관리자 | 개원성향진단",
+
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+  },
+};
+
+
+export default function AdminLayout({
+  children,
+}) {
+  return children;
+}
